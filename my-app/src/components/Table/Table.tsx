2ccd1ddfc0e4fcb583e2d1cf1a940531a2256843
welel <jocafmv@gmail.com>
2022-03-29 15:53:26 +0000

    Table Component

---
+++ my-app/src/components/Table/Table.tsx
@@ -0,0 +1,117 @@
+import { iProductsList } from '../../interfaces/iProductsList';
+import { ProductService } from '../../services';
+import React, { useState, useEffect } from 'react';
+import './Table.css';
+
+function Table() {
+
+    const [data, setData] = useState<iProductsList[]>([]);
+    const [totalPages, setTotalPages] = useState(0);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [entries, setEntries] = useState(5);
+    const [search, setSearch] = useState("");
+    const [searchBy, setSearchBy] = useState("");
+
+    const service = new ProductService();
+
+    const thead = [
+        {
+            name: "Name"
+        },
+        {
+            name: "Description"
+        },
+        {
+            name: "Type"
+        },
+        {
+            name: "Quantity"
+        },
+        {
+            name: "Price"
+        }
+    ];
+
+
+
+    function CheckPages(): JSX.Element {
+        if (currentPage <= 1 && currentPage + 1 > totalPages) {
+          return <div className="btn-currentPage">
+            <button onClick={() => setCurrentPage(currentPage - 1)} disabled>Previous</button>
+            <div className='currentPageInput'>{currentPage}</div>
+            <button onClick={() => setCurrentPage(currentPage + 1)} disabled>Next</button>
+          </div>;
+        }
+    
+    
+        if (currentPage + 1 > totalPages) {
+          return <div className="btn-currentPage">
+            <button onClick={() => setCurrentPage(currentPage - 1)}>Previous</button>
+            <div className='currentPageInput'>{currentPage}</div>
+            <button onClick={() => setCurrentPage(currentPage + 1)} disabled>Next</button>
+          </div>;
+        }
+    
+        if (currentPage <= 1) {
+          return <div className="btn-currentPage">
+            <button onClick={() => setCurrentPage(currentPage - 1)} disabled>Previous</button>
+            <div className='currentPageInput'>{currentPage}</div>
+            <button onClick={() => setCurrentPage(currentPage + 1)}>Next</button>
+          </div>;
+        }
+    
+        if (currentPage) {
+          return <div className="btn-currentPage">
+            <button onClick={() => setCurrentPage(currentPage - 1)}>Previous</button>
+            <div className='currentPageInput'>{currentPage}</div>
+            <button onClick={() => setCurrentPage(currentPage + 1)}>Next</button>
+          </div>;
+        }
+        return <div>
+            <p>Erro</p>
+        </div>;
+      }
+
+      useEffect(() => {
+        service.getAll().then(result => {
+            setTotalPages(result.pages);
+            setCurrentPage(result.currentPage);
+            setEntries(result.entries);
+            setSearch(result.search);
+            setSearchBy(result.searchBy);
+            setData(result.products);
+            console.log("Info: ");
+            console.log(totalPages);
+            console.log(currentPage);
+            console.log(result);
+        });
+    }, []);
+
+  return (
+    <div className="content-table">
+        <table>
+            <thead>
+                <tr>
+                    {thead.map((item, index) => {
+                        return <th key={index}>{item.name}</th>
+                    })}
+                </tr>
+            </thead>
+            <tbody>
+                {data.map(item => (
+                    <tr key={item.id}>
+                        <td>{item.name}</td>
+                        <td>{item.description}</td>
+                        <td>{item.type}</td>
+                        <td>{item.quantity}</td>
+                        <td>{item.price}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+        <CheckPages />
+    </div>
+  )
+}
+
+export default Table
--- my-app/src/pages/Products.tsx
+++ my-app/src/pages/Products.tsx
@@ -2,46 +2,13 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { iProductsList } from '../interfaces/iProductsList';
-import { ProductService } from '../services';
+import Table from '../components/Table/Table';
+
 function Products() {
-
-    // const [data, setData] = useState<iProductsList[]>([]);
-    const [totalPages, setTotalPages] = useState(0);
-    const [currentPage, setCurrentPage] = useState(0);
-    const [entries, setEntries] = useState(5);
-    const [search, setSearch] = useState("");
-    const [searchBy, setSearchBy] = useState("");
-
-    const service = new ProductService();
-
-    service.getAll().then(result => {
-        setTotalPages(result.pages);
-        setCurrentPage(result.currentPage);
-        setEntries(result.entries);
-        setSearch(result.search);
-        setSearchBy(result.searchBy);
-        // setData(result.products);
-    });
-
-    console.log(totalPages);
 
     return (
         <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Ola</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {/* {produtos.map(item => {
-                        <tr key={item.id}>
-                            <td>{item.name}</td>
-                        </tr>
-                    }} */}
-                </tbody>
-            </table>
+            <Table />
         </div>
     )
 }
